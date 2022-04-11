41a115b78209c35f64de46b71f241d0b15b74913
walter losada <walylosada@gmail.com>
2022-04-11 17:12:23 -0500

    homework commit

--- FT-M1-master/00-IntroToCS/homework/homework.js
+++ FT-M1-master/00-IntroToCS/homework/homework.js
@@ -2,25 +2,24 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+
   var acum = 0;
   for (var i = 0; i < num.length; i++) {
-    acum = acum + num[i] ** (num.length - 1 - i);
+    acum+= num[i] * 2 ** (num.length - 1 - i);
     }
     return acum;
   }
   
   function DecimalABinario(num) {
     // tu codigo aca
-  var str = '' ;
-  //var arr =[];  
+  
+  var array =[];  
   while(num > 0){
-    var resto = num % 2;
-    str = resto + str;
-    //arr.unshift(resto);
+    array.unshift(num%2);
     num = Math.floor(num/2);
-  }
-  return str;
-  }
+      }
+return array.join('')
+    }
 
 
 module.exports = {
