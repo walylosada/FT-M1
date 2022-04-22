6e361d5d26e47b8ce3b220382e5a4d6f290050fe
walter losada <walylosada@gmail.com>
2022-04-22 17:18:31 -0500

    homework commit

--- FT-M1-master/03-EstructuraDeDatos-I/homework/homework.js
+++ FT-M1-master/03-EstructuraDeDatos-I/homework/homework.js
@@ -7,6 +7,9 @@ function nFactorial(n) {
   // devolvé el factorial de n (n!)
   // ej:
   // el factorial de 3 es 6 (3 * 2 * 1)
+  if(n < 0) throw new Error("No puedo tener negativos");
+  if(n === 0 || n === 1) return 1;
+  return n*nFactorial(n-1);
 }
 
 function nFibonacci(n) {
@@ -15,7 +18,9 @@ function nFibonacci(n) {
   // nFibonacci(0) // 0  // el elemento 0 es cero
   // nFibonacci(1) // 1 // el elemento 1 es 1
   // nFibonacci(6) // 1 // el elemento 6 es 8
-
+if(n===0) return 0;
+if(n===1) return 1;
+return nFibonacci(n-1) +nFibonacci(n-2);
 }
 
 // Para esta parte no es necesario utilizar recursión.
--- FT-M1-master/07-Algoritmos-II/homework/homework.js
+++ FT-M1-master/07-Algoritmos-II/homework/homework.js
@@ -6,7 +6,22 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  if(array.length <= 1) return array;
+  let pivot = array[Math.floor(Math.random() * array.length)];
+  let left = [];
+  let right = [];
+  let equal =[];
+  for (let i = 0; i < array.length; i++) {
+  if(array[i] < pivot){
+    left.push(array[i]);
+  }else if(array[i]> pivot){
+     right.push(array[i]);
+  }else {
+    equal.push(array[i]);
+  }
+  
+}
+return quickSort(left).concat(equal).concat(quickSort(right));
 }
 
 function mergeSort(array) {
@@ -14,7 +29,33 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+ if(array.length === 1) return array;
+ let divido = dividir(array);
+ let left = divido[0];
+ let right = divido[1];
+ return unir(mergeSort(left), mergeSort(right));
+}
+function dividir(array){
+  let mitad = Math.floor(array.length/2);
+  let left = array.slice(0, mitad);
+  let right = array.slice(mitad);
+  return [left, right];
+}
 
+function unir(left, right){
+  let leftIndex =0;
+  let rightIndex =0;
+  let array =[];
+  while(leftIndex < left.length && rightIndex < right.length){
+   if(left[leftIndex] < right[rightIndex]){
+   array.push(left[leftIndex]);
+   leftIndex++;
+  }else{
+    array.push(right[rightIndex]);
+    rightIndex++;
+  }
+}
+ return array.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 }
 
 // No modificar nada debajo de esta línea
